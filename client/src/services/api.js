d4cd092dc557a14db1abea99bd4cd860ce0807af
hellofarooqui <farooqui.hello@gmail.com>
2025-10-30 07:39:44 +0400

    update env file in frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const server = import.meta.env.VITE_SERVER_URL;
+
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `'${server}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
